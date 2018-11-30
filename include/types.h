97009cb0cd111d07b01aa10c6ad8d21bcae3dc4a
Salvatore Maraniello <salvatore.maraniello10@imperial.ac.uk>
2018-11-30 19:17:43 +0000

    Added linear UVLM routines
    Routines added under UVLMlin namespace

--- include/types.h
+++ include/types.h
@@ -368,4 +368,29 @@ namespace UVLM
 }
 
 
+
+/*
+Define types for C++ interface on linear UVLM routines.
+Matrices size is specified whenever possible to maximise speed. 
+*/
+
+namespace UVLMlin{
+
+    using namespace Eigen;
+
+    typedef Matrix<double,4,3,RowMajor> Matrix4by3d;
+
+    // map 1d arrays into Eigen Matrices (interface for 1D or 2D python arrays)
+    typedef Map< Matrix<double,Dynamic,Dynamic,RowMajor> > map_Mat; 
+    typedef Map< Matrix<double,4,3,RowMajor> > map_Mat4by3; 
+    typedef Map< Matrix<double,3,3,RowMajor> > map_Mat3by3; 
+    typedef Map< Matrix<double,1,3> > map_RowVec3; 
+
+    // mapp 3D python arrays
+    typedef std::vector<map_Mat3by3> Vec_map_Mat3by3;
+    typedef std::vector<map_Mat> Vec_map_Mat;
+}
+
+
+
 #include "typeutils.h"
