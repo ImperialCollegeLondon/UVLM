16dbcad81a12827efa3bef80378e47eb292ece27
Salvatore Maraniello <salvatore.maraniello10@imperial.ac.uk>
2019-03-04 15:16:00 +0000

    Removed namespace UVLMlin from cpp_interface

--- src/cpp_interface.cpp
+++ src/cpp_interface.cpp
@@ -650,10 +650,10 @@ DLLEXPORT void run_SHW
     );
 }
 
-// linear UVLM interface
-namespace UVLMlin{
 
-  extern "C" void call_der_biot_panel(double p_DerP[9],
+// linear UVLM interface
+
+DLLEXPORT void call_der_biot_panel(double p_DerP[9],
                     double p_DerVertices[36],// 4x9
                     double p_zetaP[3],
                     double p_ZetaPanel[12],
@@ -664,22 +664,22 @@ namespace UVLMlin{
 
     int vv;
 
-    map_Mat3by3 DerP(p_DerP);
-    Vec_map_Mat3by3 DerVertices; // requires push_back to assigns
-    const map_RowVec3 zetaP(p_zetaP);
-    const map_Mat4by3 ZetaPanel(p_ZetaPanel);
+    UVLMlin::map_Mat3by3 DerP(p_DerP);
+    UVLMlin::Vec_map_Mat3by3 DerVertices; // requires push_back to assigns
+    const UVLMlin::map_RowVec3 zetaP(p_zetaP);
+    const UVLMlin::map_Mat4by3 ZetaPanel(p_ZetaPanel);
 
     // initialise DerVertices - all done by reference
     for(vv=0;vv<4;vv++){
-      DerVertices.push_back( map_Mat3by3(p_DerVertices+9*vv) );
+      DerVertices.push_back( UVLMlin::map_Mat3by3(p_DerVertices+9*vv) );
     }
 
-    der_biot_panel_map( DerP, DerVertices, zetaP, ZetaPanel, gamma );
+    UVLMlin::der_biot_panel_map( DerP, DerVertices, zetaP, ZetaPanel, gamma );
   }
 
 
 
-  extern "C" void call_biot_panel(double p_vel[3],
+DLLEXPORT void call_biot_panel(double p_vel[3],
                   double p_zetaP[3],
                   double p_ZetaPanel[12],
                   const double& gamma ){
@@ -688,16 +688,16 @@ namespace UVLMlin{
     into 1d arrays.
     */
 
-    map_RowVec3 velP(p_vel);
-    const map_RowVec3 zetaP(p_zetaP);
-    const map_Mat4by3 ZetaPanel(p_ZetaPanel);
+    UVLMlin::map_RowVec3 velP(p_vel);
+    const UVLMlin::map_RowVec3 zetaP(p_zetaP);
+    const UVLMlin::map_Mat4by3 ZetaPanel(p_ZetaPanel);
 
-    biot_panel_map(velP, zetaP, ZetaPanel, gamma);
+    UVLMlin::biot_panel_map(velP, zetaP, ZetaPanel, gamma);
   }
 
 
 
-  extern "C" void call_dvinddzeta(double p_DerC[9],
+DLLEXPORT void call_dvinddzeta(double p_DerC[9],
                   double p_DerV[],
                   double p_zetaC[3],
                   double p_ZetaIn[],
@@ -713,27 +713,26 @@ namespace UVLMlin{
     int Kzeta_in_bound=(M_in_bound+1)*(N_in+1);
 
     // interface
-    map_Mat3by3 DerC(p_DerC);
-    const map_RowVec3 zetaC(p_zetaC);
+    UVLMlin::map_Mat3by3 DerC(p_DerC);
+    const UVLMlin::map_RowVec3 zetaC(p_zetaC);
 
-    map_Mat DerV(p_DerV,3,3*Kzeta_in_bound);
-    map_Mat GammaIn(p_GammaIn,M_in,N_in);
+    UVLMlin::map_Mat DerV(p_DerV,3,3*Kzeta_in_bound);
+    UVLMlin::map_Mat GammaIn(p_GammaIn,M_in,N_in);
 
-    Vec_map_Mat ZetaIn;
+    UVLMlin::Vec_map_Mat ZetaIn;
     for(cc=0;cc<3;cc++){
-      ZetaIn.push_back( map_Mat(p_ZetaIn+cc*Kzeta_in, M_in+1, N_in+1) );
+      ZetaIn.push_back( UVLMlin::map_Mat(p_ZetaIn+cc*Kzeta_in, M_in+1, N_in+1) );
     }
 
-    dvinddzeta( DerC,DerV,
+    UVLMlin::dvinddzeta( DerC,DerV,
           zetaC,ZetaIn,GammaIn,
           M_in,N_in,Kzeta_in,
           IsBound,M_in_bound,Kzeta_in_bound);
-
   }
 
 
 
-  extern "C" void call_aic3(  double p_AIC3[],
+DLLEXPORT void call_aic3(  double p_AIC3[],
                 double p_zetaC[3],
                 double p_ZetaIn[],
                 int& M_in,
@@ -742,20 +741,21 @@ namespace UVLMlin{
     int cc;
     int K_in=M_in*N_in;
 
-    map_Mat AIC3(p_AIC3,3,K_in);
-    const map_RowVec3 zetaC(p_zetaC);
+    UVLMlin::map_Mat AIC3(p_AIC3,3,K_in);
+    const UVLMlin::map_RowVec3 zetaC(p_zetaC);
 
     int Kzeta_in=(M_in+1)*(N_in+1);
-    Vec_map_Mat ZetaIn;
+    UVLMlin::Vec_map_Mat ZetaIn;
     for(cc=0;cc<3;cc++){
-      ZetaIn.push_back( map_Mat(p_ZetaIn+cc*Kzeta_in, M_in+1, N_in+1) );
+      ZetaIn.push_back( UVLMlin::map_Mat(p_ZetaIn+cc*Kzeta_in, M_in+1, N_in+1) );
     }
 
-    aic3(AIC3, zetaC, ZetaIn, M_in, N_in);
+    UVLMlin::aic3(AIC3, zetaC, ZetaIn, M_in, N_in);
   }
-}
 
- DLLEXPORT void call_ind_vel(
+
+
+DLLEXPORT void call_ind_vel(
                 double p_vel[3],
                 double p_zetaC[3],
                 double p_ZetaIn[],
