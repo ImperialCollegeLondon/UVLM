43c633b72cf8edbd51768c47ecc2ddbea0fa84cb
Alfonso del Carre <fonsocarre@gmail.com>
2019-08-09 19:58:46 +0100

    Revert "First test of biotsavart with no eigen and pure C" Wrong branch
    This reverts commit d133570a0444ba29b0c02cba89da6e834cf8e279.

--- include/constants.h
+++ include/constants.h
@@ -10,7 +10,6 @@ namespace UVLM
         const unsigned int NDIM = 3;
         const UVLM::Types::Real PI = 3.1415926535897932384626433832795028841971;
         const UVLM::Types::Real PI4 = 4.0*PI;
-        const UVLM::Types::Real INV_PI4 = 1.0/(4.0*PI);
         const UVLM::Types::Real DEGREES2RAD = PI/180.0;
 
         const UVLM::Types::Real EPSILON =
