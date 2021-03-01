96319b5e0f992c78c76c9d503757b6dae662c687
sduess <s.dussler20@imperial.ac.uk>
2021-03-01 16:41:39 +0000

    add(phantom): extend phantom panels to symmetry plane

--- include/phantom.h
+++ include/phantom.h
@@ -38,13 +38,15 @@ void UVLM::Phantom::create_phantom_zeta
 )
 {
     const uint n_surf = zeta.size();
-
+    uint N_col;
+    float phantom_dy=0.0;
     zeta_phantom.resize(n_surf);
     unsigned int N_phantom_cell, N_row_phantom, index_phantom;
     for(uint i_surf=0; i_surf<n_surf; i_surf++)
     {
         N_phantom_cell = (flag_zeta_phantom[i_surf] > 0).count();
         N_row_phantom = zeta[i_surf][0].rows();
+ 
         zeta_phantom[i_surf].resize(3);
         if(N_phantom_cell != 0)
         {
@@ -53,30 +55,50 @@ void UVLM::Phantom::create_phantom_zeta
             {
                 if(flag_zeta_phantom[i_surf](i))
                 {
+                    // To-DO: Here only one junction is assumeds
                     index_phantom = i;
                     break;
                 }
+
+            }
+            
+            // Required phantom panels to extend phantom panels to symmetry plane
+           N_col = abs(round(zeta[i_surf][1](0, index_phantom)/(zeta[i_surf][1](0, index_phantom+1)-zeta[i_surf][1](0, index_phantom))));
+            if(N_col==1)
+            {
+                phantom_dy=zeta[i_surf][1](0, index_phantom+1)-zeta[i_surf][1](0, index_phantom);
+            }
+            else
+            {
+                phantom_dy=zeta[i_surf][1](0, index_phantom)/N_col;
             }
             for(uint i_dim=0; i_dim<3; ++i_dim)
             {
-                zeta_phantom[i_surf][i_dim].resize(N_row_phantom, N_phantom_cell+1);
-
-                zeta_phantom[i_surf][i_dim].block(0, index_phantom,N_row_phantom, 1) = zeta[i_surf][i_dim].block(0, index_phantom,N_row_phantom, 1);
-                if(i_dim==1)
+                zeta_phantom[i_surf][i_dim].resize(N_row_phantom, N_col+1);
+                
+                zeta_phantom[i_surf][i_dim].block(0, 0,N_row_phantom, 1) = zeta[i_surf][i_dim].block(0, index_phantom,N_row_phantom, 1);
+                
+                for(uint i_col = 1;  i_col<N_col+1;++i_col)
                 {
-                   //TO-DO:
-                   // - Symmetry plane is used for end of panel
-                   // - Here symmetry plane is assumed to be at y = 0 --> adjust e.g. if sideslip angle != 0
-                   for(uint i_row=0;i_row<N_row_phantom;++i_row)
-                   {
-                       zeta_phantom[i_surf][i_dim].col(1) = 2*zeta[i_surf][i_dim].block(0, index_phantom, N_row_phantom,1)-zeta[i_surf][i_dim].block(0, index_phantom+1, N_row_phantom,1);
-                   }
-                }
-                else
-                {      
-                    // TO-DO:
-                    // - here only unswept wings are covered
-                    zeta_phantom[i_surf][i_dim].block(0, index_phantom+1,N_row_phantom, 1) = zeta_phantom[i_surf][i_dim].block(0, index_phantom,N_row_phantom, 1);
+                    
+                    if(i_dim==1)
+                    {
+                       //TO-DO:
+                       // - Symmetry plane is used for end of panel
+                       // - Here symmetry plane is assumed to be at y = 0 --> adjust e.g. if sideslip angle != 0
+                       
+                       zeta_phantom[i_surf][i_dim].col(i_col) = zeta_phantom[i_surf][i_dim].col(i_col-1);
+                       for(uint i_row=0;i_row<N_row_phantom;++i_row)
+                       {
+                           zeta_phantom[i_surf][i_dim](i_row, i_col)-=phantom_dy;
+                        }
+                    }
+                    else
+                    {      
+                        // TO-DO:
+                        // - here only unswept wings are covered
+                        zeta_phantom[i_surf][i_dim].col(i_col) = zeta_phantom[i_surf][i_dim].col(index_phantom);
+                    }
                 }
             }
         
