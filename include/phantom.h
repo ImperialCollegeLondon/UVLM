4d211868aa2300de8cc6cc198d90c2c0a984efbf
sduess <s.dussler20@imperial.ac.uk>
2021-04-06 09:11:41 +0100

    fix [phantom]: adjust phantom panel creation
    - ensure same panel size at junction
    - force y coordinate to be zero at symmetry plane

--- include/phantom.h
+++ include/phantom.h
@@ -47,10 +47,11 @@ void UVLM::Phantom::create_phantom_zeta
 )
 {
     const uint n_surf = zeta.size();
-    uint N_col;
+    int N_col, i_col;
     float phantom_dy=0.0;
     zeta_phantom.resize(n_surf);
     unsigned int N_phantom_cell, N_row_phantom;
+    std::cout <<flag_zeta_phantom;
     for(uint i_surf=0; i_surf<n_surf; i_surf++)
     {
         zeta_phantom[i_surf].resize(3);
@@ -58,36 +59,39 @@ void UVLM::Phantom::create_phantom_zeta
         //To-Do: Check why -1 necessary (bug probably in datastructres.py or aerogrid.py)
         for(uint index_phantom=0;index_phantom<zeta[0][0].cols()-1;++index_phantom)
         {
-            //To-Do: Add case no junction exists
             if (flag_zeta_phantom(index_phantom, i_surf)==1)
             {
-                // Required phantom panels to extend phantom panels to symmetry plane
-                //N_col = 1;
-                N_col = abs(round(zeta[i_surf][1](0, index_phantom)/(zeta[i_surf][1](0, index_phantom+1)-zeta[i_surf][1](0, index_phantom))));
-                if(N_col==1)
-                {
-                    phantom_dy=zeta[i_surf][1](0, index_phantom+1)-zeta[i_surf][1](0, index_phantom);
-                }
-                else
-                {
-                    phantom_dy=zeta[i_surf][1](0, index_phantom)/N_col;
-                }
+                phantom_dy= zeta[i_surf][1](0, index_phantom+1)-zeta[i_surf][1](0, index_phantom);
+                N_col = abs(round(zeta[i_surf][1](0, index_phantom)/phantom_dy));
+                
                 for(uint i_dim=0; i_dim<3; ++i_dim)
                 {
                     zeta_phantom[i_surf][i_dim].resize(N_row_phantom, N_col+1);
-                    zeta_phantom[i_surf][i_dim].block(0, 0,N_row_phantom, 1) = zeta[i_surf][i_dim].block(0, index_phantom,N_row_phantom, 1);
                     
-                    for(uint i_col = 1;  i_col<N_col+1;++i_col)
+                    zeta_phantom[i_surf][i_dim].block(0, 0,N_row_phantom, 1) = zeta[i_surf][i_dim].block(0, index_phantom,N_row_phantom, 1);
+                    i_col = N_col;
+                    for(i_col = N_col;  i_col>=0;--i_col)
                     {
                         if(i_dim==1)
                         {
                             //TO-DO:
                             // - Symmetry plane is used for end of panel
                             // - Here symmetry plane is assumed to be at y = 0 --> adjust e.g. if sideslip angle != 0
-                            zeta_phantom[i_surf][i_dim].col(i_col) = zeta_phantom[i_surf][i_dim].col(i_col-1);
                             for(uint i_row=0;i_row<N_row_phantom;++i_row)
                             {
-                                zeta_phantom[i_surf][i_dim](i_row, i_col)-=phantom_dy;
+                                if(i_col==0 && N_col>1)
+                                {
+                                    zeta_phantom[i_surf][i_dim](i_row, i_col) = 0.0;
+                                }
+                                else if(i_col==N_col)
+                                {
+                                     zeta_phantom[i_surf][i_dim](i_row, i_col) =  zeta[i_surf][i_dim](i_row, index_phantom);
+                                }
+                                else
+                                {
+
+                                    zeta_phantom[i_surf][i_dim](i_row, i_col) = zeta_phantom[i_surf][i_dim](i_row, i_col+1) -phantom_dy;
+                                }
                             }
                         }
                         else
