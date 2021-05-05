ff54d956ae221c92807cd86b3befa496f9cd9f2f
sduess <s.dussler20@imperial.ac.uk>
2021-05-05 18:06:30 +0100

    feature: structs to store parameters for individual surface types
    - Base struct "surface" and inherited classes for nonlifting, phantom, and lifting surfaces (steady and unsteady)
    - move mapping functions to mapping.h- move mapping functions to mapping.h

--- include/mapping.h
+++ include/mapping.h
@@ -39,7 +39,79 @@ namespace UVLM
                                             out[idim]);
             }
         }
+        void map_VecVecMat(const UVLM::Types::VecDimensions& dimensions,
+                           double** in,
+                           UVLM::Types::VecVecMapX& map,
+                           const int& correction=0,
+                           const unsigned int& n_dim=UVLM::Constants::NDIM)
+        {
+            const unsigned int n_surf = dimensions.size();
+            map.resize(n_surf);
+            unsigned int counter = 0;
+            for (uint i_surf=0; i_surf<n_surf; ++i_surf)
+            {
+                for (uint i_dim=0; i_dim<n_dim; ++i_dim)
+                {
+                    map[i_surf].push_back(UVLM::Types::MapMatrixX (in[counter],
+                                                                   dimensions[i_surf].first + correction,
+                                                                   dimensions[i_surf].second + correction));
+                    counter++;
+                }
+            }
+        }
 
+        void map_VecMat(const UVLM::Types::VecDimensions& dimensions,
+                        double** in,
+                        UVLM::Types::VecMapX& map,
+                        const int& correction=0)
+        {
+            const unsigned int n_surf = dimensions.size();
+            for (unsigned int i_surf=0; i_surf<n_surf; ++i_surf)
+            {
+                map.push_back(UVLM::Types::MapMatrixX (in[i_surf],
+                                                       dimensions[i_surf].first + correction,
+                                                       dimensions[i_surf].second + correction));
+            }
+        }
+
+        void map_VecVec1(const UVLM::Types::VecDimensions& dimensions,
+                        double** in,
+                        UVLM::Types::VecMapVX& map,,
+                        const int& correction=0)
+        {
+            // Generates a variable that will be indexed as map[i_surf](i_m)
+            const unsigned int n_surf = dimensions.size();
+            for (unsigned int i_surf=0; i_surf<n_surf; ++i_surf)
+            {
+                map.push_back(UVLM::Types::MapVectorX (in[i_surf],
+                                                       dimensions[i_surf].first + correction));
+            }
+        }
+        void map_VecX(const uint N_rows,
+                        double* in,
+                        UVLM::Types::VectorX& out,
+                        //UVLM::Types::MapVectorX& map,
+                        const int& correction=0)
+        {
+            // Caution: Use map VecX only for small vectors like p_rbm_vel_g
+            out.resize(N_rows);
+            for (uint i_row=0; i_row < N_rows; ++i_row)
+            {
+                out[i_row] = in[i_row];   
+            }
+        }
+
+        void transform_dimensions(unsigned int& n_surf,
+                                  unsigned int** dimensions_in,
+                                  UVLM::Types::VecDimensions& dimensions)
+        {
+            dimensions.resize(n_surf);
+            for (unsigned int i_surf=0; i_surf<n_surf; ++i_surf)
+            {
+                dimensions[i_surf].first = dimensions_in[i_surf][0];
+                dimensions[i_surf].second= dimensions_in[i_surf][1];
+            }
+        }
 
     }
 }
